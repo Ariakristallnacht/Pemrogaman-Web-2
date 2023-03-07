a3dada224969f8d9a49056cf3181754b3b2adc1d
Ariakristallnacht <ariakristallnacht@gmail.com>
2023-03-07 15:33:37 +0700

    upload praktikum 3

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,22 @@
+<?php 
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital",
+];
+
+$skills = [
+    "HTML" =>10,
+    "CSS" =>10,
+    "Javasccript" =>20,
+    "RWD Bootsrap" =>20,
+    "PHP" =>30,
+    "Pyhton"=>30,
+    "Java"=>50,
+];
+
+
+
+?>
